180faa864a149ffec31b96bffda139929ab75540
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-06-10 12:18:01 -0300

    -Variable restrictions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
 fun main() {
     println("Hello World")
+    val titular: String = "Ricardo"
+    val numeroConta: Int = 8020-9
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
+    println("Titular: $titular")
+    println("Número da Conta: $numeroConta")
+    println("Saldo: $saldo")
 }

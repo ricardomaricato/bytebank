0de0f850e5533d7796ba8357bd8a7f7bd917262e
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-06-11 14:53:47 -0300

    -Inheritance

---
+++ src/main/kotlin/Diretor.kt
@@ -0,0 +1,12 @@
+class Diretor(
+    nome: String,
+    cpf: String,
+    salario: Double,
+    val senha: Int,
+    val plr: Double
+) : Funcionario(
+    nome = nome,
+    cpf = cpf,
+    salario = salario
+) {
+}
---
+++ src/main/kotlin/Gerente.kt
@@ -0,0 +1,11 @@
+class Gerente(
+    val senha: Int,
+    nome: String,
+    cpf: String,
+    salario: Double
+) : Funcionario(
+    nome = nome,
+    cpf = cpf,
+    salario = salario
+) {
+}

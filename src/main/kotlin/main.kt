ed1c1dd7ccaa3515067f17ae48827aed27a03098
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-06-11 12:53:35 -0300

    -Creating a new class

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,15 @@
 fun main() {
-    testaComportamentosConta()
+    println("Bem vindo ao bytebank")
+
+    val alex = Funcionario(
+        nome = "Alex",
+        cpf = "111.111.111-11",
+        salario = 1000.0
+    )
+
+    println("nome ${alex.nome}")
+    println("cpf ${alex.cpf}")
+    println("salário ${alex.salario}")
+    println("bonificação ${alex.bonificacao()}")
 }
 
